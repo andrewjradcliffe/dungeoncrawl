1b6889a31d9785184a8cba56613f6d0334d3265a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-07-11 20:11:28 -0700

    Expand healing spells

--- src/spell.rs
+++ src/spell.rs
@@ -6,7 +6,8 @@ use std::str::FromStr;
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
 pub enum Spell {
-    Heal,
+    Cure1,
+    Cure2,
     Fire,
     Stone,
 }
@@ -15,7 +16,8 @@ pub use Spell::*;
 impl Spell {
     pub fn cost(&self) -> i64 {
         match self {
-            Heal => 25,
+            Cure1 => 10,
+            Cure2 => 25,
             Fire => 15,
             Stone => 10,
         }
@@ -23,21 +25,23 @@ impl Spell {
 
     pub fn description(&self) -> &'static str {
         match self {
-            Heal => "restores 50 HP",
+            Cure1 => "restores 25 HP",
+            Cure2 => "restores 50 HP",
             Fire => "causes 35 damage",
             Stone => "causes 25 damage",
         }
     }
     pub fn damage(&self) -> i64 {
         match self {
-            Heal => 0,
+            Cure1 | Cure2 => 0,
             Fire => 35,
             Stone => 25,
         }
     }
     pub fn healing(&self) -> i64 {
         match self {
-            Heal => 50,
+            Cure1 => 25,
+            Cure2 => 50,
             Fire => 0,
             Stone => 0,
         }
@@ -49,14 +53,19 @@ impl FromStr for Spell {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let s = s.trim();
 
-        static RE_HEAL: Lazy<Regex> = Lazy::new(|| Regex::new("(?i)^h(?:eal|ea|e)?$").unwrap());
+        static RE_CURE1: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"(?i)^(?:c\s*[1i]|cure\s*[1i])$").unwrap());
+        static RE_CURE2: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"(?i)^(?:c\s*(?:2|ii)|cure\s*(?:2|ii))$").unwrap());
 
-        static RE_FIRE: Lazy<Regex> = Lazy::new(|| Regex::new("(?i)^f(?:ire|ir|i)?$").unwrap());
+        static RE_FIRE: Lazy<Regex> = Lazy::new(|| Regex::new("(?i)^(?:fire|f)$").unwrap());
 
-        static RE_STONE: Lazy<Regex> =
-            Lazy::new(|| Regex::new("(?i)^s(?:tone|ton|to|t)?$").unwrap());
-        if RE_HEAL.is_match(s) {
-            Ok(Heal)
+        static RE_STONE: Lazy<Regex> = Lazy::new(|| Regex::new("(?i)^(?:stone|s)$").unwrap());
+
+        if RE_CURE1.is_match(s) {
+            Ok(Cure1)
+        } else if RE_CURE2.is_match(s) {
+            Ok(Cure2)
         } else if RE_FIRE.is_match(s) {
             Ok(Fire)
         } else if RE_STONE.is_match(s) {
@@ -70,7 +79,8 @@ impl FromStr for Spell {
 impl fmt::Display for Spell {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Heal => write!(f, "Heal"),
+            Cure1 => write!(f, "Cure I"),
+            Cure2 => write!(f, "Cure II"),
             Fire => write!(f, "Fire"),
             Stone => write!(f, "Stone"),
         }
@@ -78,15 +88,23 @@ impl fmt::Display for Spell {
 }
 
 pub(crate) fn spell_menu() -> Option<Spell> {
+    static RE_QUIT: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)^(?:quit|q)$").unwrap());
+
     let mut buf = String::with_capacity(1 << 7);
     println!("---- Entering spell menu... ----");
     loop {
         buf.clear();
         println!(
             "    {:<30} | {:<30} | cost: {} MP",
-            format!("{}", Heal),
-            Heal.description(),
-            Heal.cost()
+            format!("{}", Cure1),
+            Cure1.description(),
+            Cure1.cost()
+        );
+        println!(
+            "    {:<30} | {:<30} | cost: {} MP",
+            format!("{}", Cure2),
+            Cure2.description(),
+            Cure2.cost()
         );
         println!(
             "    {:<30} | {:<30} | cost: {} MP",
@@ -113,7 +131,7 @@ pub(crate) fn spell_menu() -> Option<Spell> {
 
         let s = buf.trim();
 
-        if s.eq_ignore_ascii_case("q") || s.eq_ignore_ascii_case("quit") {
+        if RE_QUIT.is_match(s) {
             break None;
         } else {
             if let Ok(spell) = s.parse::<Spell>() {
