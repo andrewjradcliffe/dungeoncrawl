a2284e3e436f4eda465e1d8043ada2d2234c35e8
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-07-26 15:36:28 -0700

    Add some tests

--- src/multiset.rs
+++ src/multiset.rs
@@ -21,6 +21,10 @@ impl<T: fmt::Debug + Clone + Copy + PartialEq + Eq + PartialOrd + Ord + Hash> Mu
     pub fn is_empty(&self) -> bool {
         self.sum == 0
     }
+    pub fn clear(&mut self) {
+        self.sum = 0;
+        self.bag.clear();
+    }
 
     pub fn pop(&mut self, item: T) -> Option<T> {
         match self.bag.entry(item) {
@@ -77,6 +81,9 @@ impl<T: fmt::Debug + Clone + Copy + PartialEq + Eq + PartialOrd + Ord + Hash> Mu
     pub fn n_available(&self, item: &T) -> usize {
         self.bag.get(item).map(Clone::clone).unwrap_or(0)
     }
+    pub fn n_total(&self) -> usize {
+        self.sum
+    }
 }
 impl<T: fmt::Debug + Clone + Copy + PartialEq + Eq + PartialOrd + Ord + Hash>
     FromIterator<(T, usize)> for MultiSet<T>
@@ -90,5 +97,71 @@ impl<T: fmt::Debug + Clone + Copy + PartialEq + Eq + PartialOrd + Ord + Hash>
             inv.push_multiple(item, count);
         }
         inv
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example() -> MultiSet<char> {
+        [('a', 1), ('b', 2), ('c', 3)].into_iter().collect()
+    }
+
+    #[test]
+    fn pop() {
+        let mut s = example();
+        s.pop('b');
+        s.pop('b');
+        assert_eq!(s.n_total(), 4);
+        assert_eq!(s.n_available(&'b'), 0);
+    }
+
+    #[test]
+    fn push() {
+        let mut s = example();
+        s.push('a');
+        assert_eq!(s.n_total(), 7);
+        assert_eq!(s.n_available(&'a'), 2);
+    }
+
+    #[test]
+    fn remove() {
+        let mut s = example();
+        s.remove('b');
+        assert_eq!(s.n_total(), 5);
+        assert_eq!(s.n_available(&'b'), 1);
+    }
+
+    #[test]
+    fn pop_multiple() {
+        let mut s = example();
+        s.pop_multiple('b', 2);
+        assert_eq!(s.n_total(), 4);
+        assert_eq!(s.n_available(&'b'), 0);
+    }
+
+    #[test]
+    fn push_multiple() {
+        let mut s = example();
+        s.push_multiple('a', 3);
+        assert_eq!(s.n_total(), 9);
+        assert_eq!(s.n_available(&'a'), 4);
+    }
+
+    #[test]
+    fn remove_multiple() {
+        let mut s = example();
+        s.remove_multiple('b', 2);
+        assert_eq!(s.n_total(), 4);
+        assert_eq!(s.n_available(&'b'), 0);
+    }
+
+    #[test]
+    fn clear() {
+        let mut s = example();
+        s.clear();
+        assert_eq!(s.n_total(), 0);
+        assert_eq!(s.n_available(&'b'), 0);
     }
 }
